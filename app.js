5165678f8cf488dc8aa3b76adb0640b2755e997b
nine-inch-rails <yan.zou22@gmail.com>
2018-01-09 19:56:12 -0500

    case insensitive

--- app.js
+++ app.js
@@ -258,7 +258,7 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    var qin = messageText.match(/wolfram (.+)/);
+    var qin = messageText.match(/wolfram (.+)/i);
     if(qin != null) {//wolfram alpha query
       var qresult = wolfram.query(qin[1], function(err, results)
           {
